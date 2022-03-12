d2d7bd9db5674e4ffebfbe83dfadaa06a362f3ce
Istiyak Hossain <istiyak.riyad@gmail.com>
2022-03-12 01:51:13 -0500

    Add Image and Data

--- server/routes/productType/create.js
+++ server/routes/productType/create.js
@@ -4,9 +4,9 @@ const {saveImage, fileFetch} = require('../../lib/imageConverter');
 
 
 
-router.post('/', fileFetch.fields([{name: 'pngImageFront', maxCount: 1}, {name: 'pngImageBack', maxCount: 1}, {name: 'layouts', maxCount: 10}]), async (req, res, next) => {
+router.post('/', fileFetch.fields([{name: 'pngImageFront', maxCount: 1}, {name: 'pngImageBack', maxCount: 1}, {name: 'layouts', maxCount: 10}, {name: 'images', maxCount: 10}]), async (req, res, next) => {
     try {
-        const {name, sizes, categoryType, price, discount} = req.body;
+        const {name, sizes, categoryType, price, discount, colors} = req.body;
 
         const frontImage = await saveImage(req.files.pngImageFront[0]);
 
@@ -22,12 +22,27 @@ router.post('/', fileFetch.fields([{name: 'pngImageFront', maxCount: 1}, {name: 
             );
         }
 
+        let imageData;
+        if(req.files.images ) {
+            if(req.files.images.length === colors.length) {
+                const images = await Promise.all(
+                    req.files.images.map(image => saveImage(image))
+                );
+                
+                imageData = images.map((image, i) => ({color: colors[i], image}))
+            }
+            else {
+                throw Error('images and colors length not matched');
+            }
+        }
+
         await new ProductType({
             name,
             sizes,
             price,
             discount,
             categoryType,
+            imageData,
             layouts: layouts && layouts.map(image => ({image})),
             pngImageFront: frontImage,
             pngImageBack: backImage
