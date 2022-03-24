8bec0d7e34871de605ab34388568bafdabb7bbb1
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2022-03-24 14:14:12 +0600

    rev

--- client/src/constants/URL.js
+++ client/src/constants/URL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://sportsveins.com:5000";
+export const BASE_URL = "https://sportsveins.com:5000";
 //export const BASE_URL = "http://meetnayeem.com:5001";
 
 export const IMAGE_PATH = `${BASE_URL}/image/`;
