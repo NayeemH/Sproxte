9b9c8d29a2d0f50c51e22dccf21169dfb8ddc988
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2022-03-25 23:55:55 +0600

    serve done

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const compression = require('compression');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 
 // Middlewares
-const middleware = require('./middleware');
-const routes = require('./routes');
+const middleware = require("./middleware");
+const routes = require("./routes");
 
 // Create express App
 const app = express();
@@ -18,36 +17,37 @@ const app = express();
 app.use(cookieParser());
 
 // Setup cors
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    credentials: true
-}));
-
+    credentials: true,
+  })
+);
 
 // Setup express middlewares'
-app.use(express.json({
+app.use(
+  express.json({
     verify: function (req, res, buf) {
-        if (req.originalUrl.startsWith('/api/v1/payment/webhook')) {
-            req.rawBody = buf.toString();
-        }
-    }
-}));
+      if (req.originalUrl.startsWith("/api/v1/payment/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Setup Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(compression());
 app.use(helmet());
 
-
 // Static files
-app.use(express.static(path.resolve('data')));
-
+app.use(express.static(path.resolve("data")));
+app.use(express.static(path.resolve("client/build")));
 
 // Main Routes
-app.use('/api/v1', routes);
-
+app.use("/api/v1", routes);
 
 // Not Found and Error handler
 app.use(middleware.notFoundError);
