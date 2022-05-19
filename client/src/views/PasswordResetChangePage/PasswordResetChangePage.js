dff09e8a02f26fcbc3fc4160ef9500bed6cf56eb
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2022-05-19 13:05:05 +0600

    auth pages done

--- client/src/views/PasswordResetChangePage/PasswordResetChangePage.js
+++ client/src/views/PasswordResetChangePage/PasswordResetChangePage.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Footer from "../../components/Footer/Footer";
-import Nav from "../../components/Nav/Nav";
+import LandingNavbar from "../../components/LandingNavbar/LandingNavbar";
 import ResetPasswordForm from "../../components/ResetPasswordForm/ResetPasswordForm";
 
 const PasswordResetChangePage = () => {
   return (
-    <div>
-      <Nav />
+    <div className="mh bg_custom">
+      <LandingNavbar page={"login"} />
       <ResetPasswordForm />
-      <Footer />
     </div>
   );
 };
