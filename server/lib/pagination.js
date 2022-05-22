9d0211c99df76b1dd09c9c069ec264b896e89826
Istiyak <istiyak.riyad@gmail.com>
2022-05-22 23:26:49 +0600

    Pagination in discover all

--- server/lib/pagination.js
+++ server/lib/pagination.js
@@ -1,8 +1,7 @@
-
-const pagination = ({page, limit}) => {
+const pagination = ({page, limit}, totalCount = 1) => {
 
     page = page > 0 ? page : 1;
-    limit = limit > 0 ? limit : 1;
+    limit = limit > 0 ? limit : totalCount;
 
     const skip = (page - 1) * limit;
 
