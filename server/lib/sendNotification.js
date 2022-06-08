2888210669e1dfef26d426538d2a23fd8127ba61
Istiyak <istiyak.riyad@gmail.com>
2022-06-09 00:30:46 +0600

    Email data

--- server/lib/sendNotification.js
+++ server/lib/sendNotification.js
@@ -1,4 +1,5 @@
 const Notification = require('../models/notification');
+const User = require('../models/user');
 
 const sendNotification = async (message, users, projectId, productId, type = 'success') => {
     try {
@@ -10,10 +11,30 @@ const sendNotification = async (message, users, projectId, productId, type = 'su
             users,
             user: users[users.length - 1]
         }).save();
+
+        const usersData = await User.find({_id: {$in: users}}, {name: 1, email: 1});
+        
+
+        // Verify Email
+        await Promise.all(usersData.map(user => {
+            return sendMail({
+                to: user.email,
+                subject: 'Notification Mail',
+                text: `This is a notification main`,
+                template: 'notification',
+                context: {
+                    username: user.name,
+                    message,
+                    link: `${CLIENT_URL}/dashboard`
+                }
+            });
+        }));
+      
     }
     catch(error) {
         console.log(error.message);
     }
 }
+
 
 module.exports = sendNotification;
